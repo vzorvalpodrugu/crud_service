7e9cdb8297878eaf3772a0864fa9df4edd19ebbd
vzorvalpodrugu <Starkov8904qw@gmail.com>
2026-09-07 14:10:15 +0300

    обновлен domain user и добавлен domain role

--- internal/domain/user_domain.go
+++ internal/domain/user_domain.go
@@ -2,10 +2,15 @@ package domain
 
 import "time"
 
+type Role struct {
+	Id   int
+	Name string
+}
 type User struct {
 	Id         int
 	Name       string
 	Email      string
+	Roles      []Role
 	Created_at time.Time
 	Updated_at time.Time
 }
