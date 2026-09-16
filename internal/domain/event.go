5c4390894a0f093c4a387c976f14a9fece2ea07b
vzorvalpodrugu <Starkov8904qw@gmail.com>
2026-09-16 17:11:40 +0300

    fix for outboxEvent

--- internal/domain/event.go
+++ internal/domain/event.go
@@ -3,12 +3,12 @@ package domain
 import "time"
 
 type OutboxEvent struct {
-	Id        int               `json:"id"`
-	EventType string            `json:"event_type"`
-	Data      map[string]string `json:"data"`
-	Status    string            `json:"status"`
-	CreatedAt time.Time         `json:"created_at"`
-	SentAt    *time.Time        `json:"sent_at"`
+	Id        int        `json:"id"`
+	EventType string     `json:"event_type"`
+	Data      []byte     `json:"data"`
+	Status    string     `json:"status"`
+	CreatedAt time.Time  `json:"created_at"`
+	SentAt    *time.Time `json:"sent_at"`
 }
 
 const (
