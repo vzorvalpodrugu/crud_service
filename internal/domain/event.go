8d2bd19de1d52e5571f5e7646711dd657dff1040
vzorvalpodrugu <Starkov8904qw@gmail.com>
2026-09-16 15:39:55 +0300

    outboxevent domain

--- internal/domain/event.go
+++ internal/domain/event.go
@@ -2,29 +2,23 @@ package domain
 
 import "time"
 
-type Event struct {
+type OutboxEvent struct {
 	Id        int               `json:"id"`
 	EventType string            `json:"event_type"`
 	Data      map[string]string `json:"data"`
 	Status    string            `json:"status"`
 	CreatedAt time.Time         `json:"created_at"`
-	SentAt    time.Time         `json:"sent_at"`
+	SentAt    *time.Time        `json:"sent_at"`
 }
 
-func NewPostEvent(
-	EventType string,
-	PostName string,
-	PostId string,
-	PostText string,
-	AuthorId string,
-) Event {
-	return Event{
-		EventType: EventType,
-		Data: map[string]string{
-			"Id":        PostId,
-			"Name":      PostName,
-			"Author_id": AuthorId,
-			"Text":      PostText,
-		},
-	}
-}
+const (
+	EventPostCreated = "post.created"
+	EventPostUpdated = "post.updated"
+	EventPostDeleted = "post.deleted"
+)
+
+const (
+	OutboxStatusPending = "PENDING"
+	OutboxStatusSent    = "SENT"
+	OutboxStatusFailed  = "FAILED"
+)
